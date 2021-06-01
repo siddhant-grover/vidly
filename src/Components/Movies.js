b4db12621c4b4a18e882f4cb1b431a91d192ee5d
Siddhant Grover <siddhantgroverboss@gmail.com>
2021-06-02 00:34:31 +0530

    raw sorting

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -4,11 +4,12 @@ import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import Paginate from "../Components/Paginate";
 import ListGroup from "./ListGroup";
-
+import _ from 'lodash'
 function Movies(props) {
   const [arr, setArr] = useState(getMovies());
   const [pageNumber, setPageNumber] = useState(1);
   let [filtered, setFiltered] = useState([]);
+  const [order,setOrder] = useState('asc')
   const moviesPerPage = 3;
   if (arr.length === 0) {
     return (
@@ -35,7 +36,6 @@ function Movies(props) {
       })
     );
     console.log(genre);
-    // setArr(filtered)
     setPageNumber(1);
   }
 
@@ -46,69 +46,87 @@ function Movies(props) {
       ? filtered.slice(startIndex, endIndex)
       : arr.slice(startIndex, endIndex);
 
+  function onSort(path){  
+    if(order==="asc"){
+      setOrder('desc')
+    }
+    else{
+      setOrder('asc')
+    }
+    filtered.length>0?setFiltered(_.orderBy(filtered, [path], [order])):setArr(_.orderBy(arr, [path], [order]))
+    console.log(path)
+  }
+
+console.log(arr)
   return (
-    <main className="container">
-      <ListGroup genreList={genreList} genreSelect={genreSelect} />
+    <>
+      <div className="row">
+        <div className="col-3">
+          <ListGroup genreList={genreList} genreSelect={genreSelect} />
+        </div>
 
-      <p>
-        Showing {filtered.length > 0 ? filtered.length : arr.length} movies in
-        the database .
-      </p>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sliceOfArray.map((movie, index) => {
-            console.log(movie);
-            return (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Heart />
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => {
-                      filtered.length > 0 &&
-                        setFiltered(
-                          filtered.filter((movie, i) => {
-                            return i !== index;
-                          })
-                        );
-
-                      setArr(
-                        arr.filter((m, i) => {
-                          return m.title !== movie.title;
-                        })
-                      );
-                      console.log(arr);
-                    }}
-                  >
-                    DELETE
-                  </button>
-                </td>
+        <div className="col">
+          <p>
+            Showing {filtered.length > 0 ? filtered.length : arr.length} movies
+            in the database .
+          </p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th onClick={()=>onSort('title')}>Title</th>
+                <th onClick={()=>onSort('genre.name')}>Genre</th>
+                <th onClick={()=>onSort('numberInStock')}>Stock</th>
+                <th onClick={()=>onSort('dailyRentalRate')}>Rate</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Paginate
-        totalMovies={filtered.length > 0 ? filtered.length : arr.length}
-        moviesPerPage={moviesPerPage}
-        paginate={paginate}
-        currentPage={pageNumber}
-      />
-    </main>
+            </thead>
+            <tbody>
+              {sliceOfArray.map((movie, index) => {
+
+                return (
+                  <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <Heart />
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger btn-sm"
+                        onClick={() => {
+                          filtered.length > 0 &&
+                            setFiltered(
+                              filtered.filter((movie, i) => {
+                                return i !== index;
+                              })
+                            );
+
+                          setArr(
+                            arr.filter((m, i) => {
+                              return m.title !== movie.title;
+                            })
+                          );
+                          console.log(arr);
+                        }}
+                      >
+                        DELETE
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <Paginate
+            totalMovies={filtered.length > 0 ? filtered.length : arr.length}
+            moviesPerPage={moviesPerPage}
+            paginate={paginate}
+            currentPage={pageNumber}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
